b43a71b7e508076f88bbe9b4027ed5488570c652
Elfiawesome23 <elfiawesome23@gmail.com>
2026-02-16 15:24:06 +0800

    Refactor rescraper initialization and cleanup code in run_rescrape.py

--- run_rescrape.py
+++ run_rescrape.py
@@ -22,12 +22,13 @@ SITE_DIR = "docs"
 
 
 async def _main() -> None:
-	# rescraper = Rescraper(
-	# 	data_dir=DATA_DIR,
-	# 	concurrency=10,
-	# 	request_delay=0.05,
-	# 	retries=3,
-	# )
+	rescraper = Rescraper(
+		data_dir=DATA_DIR,
+		concurrency=10,
+		request_delay=0.05,
+		retries=3,
+	)
+	
 	# Additional filter to only updaate those who are more than 5 day old
 	import datetime
 	def f(data: dict) -> bool:
@@ -37,20 +38,15 @@ async def _main() -> None:
 		last_update = datetime.datetime.fromisoformat(last_updated_string)
 		today = datetime.datetime.now(tz=datetime.timezone.utc)
 		
-		print(last_update, " - ",today)
-		
 		if (today - last_update) > datetime.timedelta(days=5):
 			return True
 		return False
-	from src.store import DataStore
-	ds = DataStore(DATA_DIR)
-	print(ds.list_screen_names(f))
+	
+	await rescraper.run(f)
 
-	# await rescraper.run(f)
-
-	# log.info("Regenerating static-site data ...")
-	# generate_data(data_dir=DATA_DIR, output_dir=SITE_DIR)
-	# log.info("Rescrape complete.")
+	log.info("Regenerating static-site data ...")
+	generate_data(data_dir=DATA_DIR, output_dir=SITE_DIR)
+	log.info("Rescrape complete.")
 
 
 if __name__ == "__main__":
