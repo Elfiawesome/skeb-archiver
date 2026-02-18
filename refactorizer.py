56658e8fa2b97c2f50661036e4bee9578d13957d
Elfiawesome23 <elfiawesome23@gmail.com>
2026-02-18 21:37:55 +0800

    removed old user code and data

--- refactorizer.py
+++
@@ -1,9 +0,0 @@
-with open("old-users-2.txt", "w") as f1:
-	with open("old_users.txt", "r") as f2:
-		lines = f2.readlines()
-		for line in lines:
-			parts = line.split(",")
-
-			if len(parts) >= 2:
-				if parts[1].startswith("429"):
-					f1.write(parts[0] + "\n")
