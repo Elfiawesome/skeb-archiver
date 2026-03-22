284e02586228ea333cad95b1f35af1db628fd8a1
Elfiawesome23 <elfiawesome23@gmail.com>
2026-03-22 18:39:59 +0800

    Fixed conversion script

--- testing/convert_ts_to_unix.py
+++ testing/convert_ts_to_unix.py
@@ -2,13 +2,19 @@ from pathlib import Path
 import json
 from datetime import datetime
 
-BASE_DIR = Path().resolve() / "docs" / "skeb"
+BASE_DIR = Path().resolve() / "skeb"
 
 for file in BASE_DIR.glob("*.json"):
 	first_seen: datetime = 0.0
 	last_updated: datetime = 0.0
 	d: dict = {}
 	with file.open("rb") as f: d = json.load(f)
+	if not type(d["first_seen"]) is str: 
+		print("first_seen is not a string!")
+		continue
+	if not type(d["last_updated"]) is str:
+		print("last_updated is not a string!")
+		continue
 	
 	first_seen = datetime.fromisoformat(d["first_seen"])
 	last_updated = datetime.fromisoformat(d["last_updated"])
@@ -17,13 +23,13 @@ for file in BASE_DIR.glob("*.json"):
 
 	for work_type in d["price_history"]:
 		for work_price in d["price_history"][work_type]:
+			if not type(work_price["recorded_at"]) is str: 
+				print("recorded_at is not a string!")
+				continue
 			recorded_at = datetime.fromisoformat(work_price["recorded_at"])
 			work_price["recorded_at"] = recorded_at.timestamp()
 
 	with file.open("w", encoding="utf-8") as f: json.dump(d, f)
-
-	break
-
 
 
 # 3.20 GB
