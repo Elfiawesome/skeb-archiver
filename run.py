4a138b865948f5667e79c6054410052097b2ecb7
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-05-19 01:19:33 +0800

    Added rescrape option

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from src.pipeline import Pipeline
 from src.data_store import DataStore
 from src.skeb_client import SkebClient
 from src.source.skeb_crawl_source import SkebCrawlSource
+from src.source.rescrape_source import RescrapeSource
 from src.extension.storage_plugin import StoragePlugin
 from src.extension.summary_report_plugin import SummaryReportPlugin
 from src.extension.sourcing_limit_plugin import SourcingLimitPlugin
@@ -11,6 +12,7 @@ async def main() -> None:
 	async with SkebClient() as client:		
 		orchestrator = Pipeline(DataStore("docs/skeb"), client)
 		orchestrator.sources.append(SkebCrawlSource())
+		# orchestrator.sources.append(RescrapeSource())
 		orchestrator.extensions.append(StoragePlugin())
 		# orchestrator.extensions.append(SourcingLimitPlugin(10))
 		orchestrator.extensions.append(SummaryReportPlugin())
