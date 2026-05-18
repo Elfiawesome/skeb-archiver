5c678211aaae16fc7467abc1593e9e07dbeec9f5
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-05-19 00:33:29 +0800

    Fixed name to DebugSource

--- src/source/debug_source.py
+++ src/source/debug_source.py
@@ -2,6 +2,6 @@ from typing import AsyncGenerator
 from .source import Source
 from ..context import PipelineContext
 
-class SkebCrawlSource(Source):
+class DebugSource(Source):
 	async def get_sources(self, context: PipelineContext) -> AsyncGenerator[str, None]:
 		yield "INSERT NAME HERE"
