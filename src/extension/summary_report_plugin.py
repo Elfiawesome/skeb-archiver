4647ab78981c16f7b1a1cbd9e623447b4e12a91e
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-05-19 00:20:51 +0800

    Add new summary section

--- src/extension/summary_report_plugin.py
+++ src/extension/summary_report_plugin.py
@@ -1,5 +1,5 @@
 from ..context import PipelineContext
-from ..event.event import Event, ProfilFetchedEvent, SourceRetrievedEvent, ProfileMissingEvent, EndEvent
+from ..event.event import Event, ProfilFetchedEvent, SourceRetrievedEvent, ProfileMissingEvent, ProfileTooManyRequestsFetchedEvent, EndEvent
 from .extension_plugin import ExtensionPlugin
 from ..logger import log
 
@@ -12,6 +12,7 @@ class SummaryReportPlugin(ExtensionPlugin):
 		self.profiles_fetched: int = 0
 		self.sources_retreived: int = 0
 		self.profile_missing: int = 0
+		self.profile_rate_limited: int = 0
 	
 	def on_event(self, context: PipelineContext, event: Event):
 		
@@ -23,12 +24,16 @@ class SummaryReportPlugin(ExtensionPlugin):
 		
 		if isinstance(event, ProfileMissingEvent):
 			self.profile_missing += 1
+		
+		if isinstance(event, ProfileTooManyRequestsFetchedEvent):
+		self.profile_rate_limited += 1
 
 		if isinstance(event, EndEvent):
 			log.info(" --- SUMMARY REPORT --- ")
 			log.info("  %-20s : %d", "Sources Retreived", self.sources_retreived)
 			log.info("  %-20s : %d", "Profiles Fetched", self.profiles_fetched)
 			log.info("  %-20s : %d", "Profile Missing", self.profile_missing)
+			log.info("  %-20s : %d", "Profile Rate Limited", self.profile_rate_limited)
 
 			with (context.store._root.parent / "output.txt").open("w", encoding="utf-8") as f:
 				f.write("Test output")
