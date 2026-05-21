5ce79fbcb9d51f556822cc69ec0342cd3585ce7a
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-05-21 09:38:58 +0800

    Added custom source option to the pipeline setup

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from src.skeb_client import SkebClient
 from src.source.skeb_crawl_source import SkebCrawlSource
 from src.source.rescrape_source import RescrapeSource
 from src.source.rediscover_source import RediscoverSource
+from src.source.custom_source import CustomSource
 from src.extension.storage_plugin import StoragePlugin
 from src.extension.summary_report_plugin import SummaryReportPlugin
 from src.extension.sourcing_limit_plugin import SourcingLimitPlugin
@@ -14,8 +15,9 @@ async def main() -> None:
 		orchestrator = Pipeline(DataStore("docs/skeb"), client)
 
 		orchestrator.add_source(SkebCrawlSource())
-		# orchestrator.add_source(RescrapeSource())
+		# orchestrator.add_source(RescrapeSource(stale_days=5))
 		# orchestrator.add_source(RediscoverSource())
+		# orchestrator.add_source(CustomSource(names = ()))
 		
 		orchestrator.add_extension(StoragePlugin())
 		# orchestrator.add_extension(SourcingLimitPlugin(10))
