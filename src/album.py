d35adcb401ce30314b26d4044dabd0d3472a239a
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-05-26 18:23:41 +0800

    fixed dataclass for album for default_factory

--- src/album.py
+++ src/album.py
@@ -1,11 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class Entry:
 	screen_name: str = ""
 	description_text: str = ""
-	description_meta: dict[str] | None = None
-	override_userdata: dict[str] | None = None
+	description_meta: dict[str] = field(default_factory=dict)
+	override_userdata: dict[str] = field(default_factory=dict)
 
 @dataclass
 class Album:
