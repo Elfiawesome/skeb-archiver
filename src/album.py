f6a01e402053178bf3b38367cf6aca5c45716142
elfiawesome23@gmail.com <elfiawesome23@gmail.com>
2026-05-26 18:24:23 +0800

    Fixed defualt_factory for album

--- src/album.py
+++ src/album.py
@@ -4,9 +4,9 @@ from dataclasses import dataclass, field
 class Entry:
 	screen_name: str = ""
 	description_text: str = ""
-	description_meta: dict[str] = field(default_factory=dict)
-	override_userdata: dict[str] = field(default_factory=dict)
+	description_meta: dict[str] = field(default_factory=dict[str])
+	override_userdata: dict[str] = field(default_factory=dict[str])
 
 @dataclass
 class Album:
-	entires: list[Entry] = []
+	entires: list[Entry] = field(default_factory=list[Entry])
